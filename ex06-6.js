871147185d7e5d6d714f41935357abd5371f6f23
hayatoogura <zm18005@josai.ac.jp>
2019-12-10 03:24:04 +0000

    12/10

---
+++ ex06-6.js
@@ -0,0 +1,5 @@
+var who66='world';
+function taro66(){alert(who66+'が来た');}
+function hanako66(){alert(who66+'が来た');}
+function taro66(){who66='太郎';}
+function hanako66(){who66='花子';}
---
+++ ex06-8.js
@@ -0,0 +1,3 @@
+function sayhello68(){alert('Hello,'+document.getElementById('who68').innerHTML+'!');}
+function taro68(){documentElementById('who68').innerHTML='太郎';}
+function hanako68(){documentElementById('who68').innerHTML='花子';}
